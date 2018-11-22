d2cac985353866c03a47f3990f709b1b6c091196
杨帆 <15234408101@163.com>
2018-11-22 11:37:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,96 +1,141 @@
-function Observer(o) {
-    for (let key in o) {
-        let val = o[key];
-        addDeepObserver(val);
+class Observer {
+    constructor(o) {
+        let that = this;
+        for (let key in o) {
+            let val = o[key];
+            that.addDeepObserver(val);
 
-        let dep=new Dep();
+            let dep = new Dep();
 
-        Object.defineProperty(o, key, {
-            enumerable: true,
-            get() {
-                Dep.target && dep.addSub(Dep.target);
-                return val;
-            },
-            set(newVal) {
-                if (newVal !== val) {
-                    val = newVal;
-                    // 增加监听
-                    addDeepObserver(val);
-                    dep.notify();
+            Object.defineProperty(o, key, {
+                enumerable: true,
+                get() {
+                    Dep.target && dep.addSub(Dep.target);
+                    return val;
+                },
+                set(newVal) {
+                    if (newVal !== val) {
+                        val = newVal;
+                        // 增加监听
+                        that.addDeepObserver(val);
+                        dep.notify();
+                    }
                 }
-            },
-        });
+            });
+        }
+    }
+    addDeepObserver(o) {
+        if (!!o && typeof o === "object") {
+            new Observer(o);
+        }
     }
 }
 
-function addDeepObserver(o) {
-    if (!!o && typeof o === "object") {
-        new Observer(o);
-    }
-}
-
-
-class Dep{
+class Dep {
     constructor() {
-        this.subs=[];
+        this.subs = [];
     }
     addSub(sub) {
         this.subs.push(sub);
-        console.log(this.subs);
     }
     notify() {
-        this.subs.forEach(watch=> {
+        this.subs.forEach(watch => {
             watch.update();
         });
     }
 }
-Dep.target=null;
+Dep.target = null;
 
-class Watcher{
-    constructor(vm,wacthDist) {
-        this.watchs=[];
-        this.vm=vm;
-        Dep.target=this;
-        Object.keys(wacthDist).forEach(key=>{
-            this.watchs.push({
-                key:key,
-                fn:wacthDist[key],
+class Watcher {
+    constructor(vm, key, cb) {
+        this.watchs = [];
+        this.vm = vm;
+        this.cb = cb;
+        this.getter = this.parseGetter(key);
+        Dep.target = this;
+        this.oldVal = this.get();
+        Dep.target = null;
+    }
+    parseGetter(key) { // "b.c"
+        let keyList = key.split(".");
+        return function (obj) {
+            let data = obj;
+            let val;
+            if (keyList.length === 1) {
+                return data[keyList[0]];
+            }
+            for (let i = 0, len = keyList.length; i < len; i++) {
+                let k = keyList[i];
+                if (data[k] && typeof data[k] === "object") {
+                    data = data[k];
+                } else {
+                    val = data[k];
+                }
+            }
+            return val;
+        }
+    }
+    get() {
+        let value = this.getter(this.vm.$data);
+        return value;
+    }
+    update() {
+        let oldVal = this.oldVal;
+        let val = this.get();
+        if (val !== oldVal) {
+            this.cb(val, oldVal);
+            this.oldVal = val;
+        }
+    }
+}
+
+class Yue {
+    constructor(opts) {
+        let vm = this;
+        // 初始化数据
+        new Observer(opts.data);
+        // 挂载data
+        vm.$data = opts.data;
+        // 代理data
+        this.proxyData(opts.data);
+        // 初始化watch
+        for (let getter in opts.watch) {
+            let cb = opts.watch[getter];
+            new Watcher(vm, getter, cb);
+        }
+    }
+    proxyData(data) {
+        Object.keys(data).forEach(k => {
+            Object.defineProperty(this, k, {
+                get() {
+                    return data[k];
+                },
+                set(newVal) {
+                    if (newVal !== data[k]) {
+                        data[k] = newVal;
+                    }
+                }
             });
         });
     }
-    update() {
-        this.watchs.forEach(w=>{
-            let data=this.vm.$data[w.key];
-            w.fn(data);
-        });
-    }
 }
 
-class Yue{
-   constructor(opts) {
-       let vm=this;
-       // 初始化数据
-       new Observer(opts.data);
-       // 初始化watch
-       new Watcher(vm,opts.watch);
-       // 挂载data
-       vm.$data=opts.data;
-   } 
-}
-
-
-let y=new Yue({
-    data:{
+let y = new Yue({
+    data: {
         a: 1,
         b: {
             c: 1
         }
     },
-    watch:{
-        "a":function (val,oldVal) {
-            console.warn(val,oldVal);
+    watch: {
+        a: function (val, oldVal) {
+            console.log(val, oldVal);
+        },
+        b: function (val, oldVal) {
+            console.log(val, oldVal);
+        },
+        "b.c": function (val, oldVal) {
+            console.log(val, oldVal);
         }
     }
 });
-
-y.$data.a=2;
